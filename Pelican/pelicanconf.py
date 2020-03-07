9554a27d64fe4b4af8cce1bcd0b8d23e7fad5b49
Peter Natale <hudsonvalleygb@gmail.com>
2020-03-06 22:59:59 -0500

    Tries to fix https request to getsimpleform.com Tries to fix error with thank you redirect page

--- Pelican/pelicanconf.py
+++ Pelican/pelicanconf.py
@@ -4,9 +4,9 @@ from __future__ import unicode_literals
 
 AUTHOR = 'HVGB'
 SITENAME = 'Hudson Valley Ghostbusters'
-SITEURL = 'https://hudsonvalleyghostbusters.org'
+# SITEURL = 'https://hudsonvalleyghostbusters.org'
 # SITEURL = 'hudsonvalleygb.github.io'
-# SITEURL = ''
+SITEURL = ''
 SITE_SUMMARY = 'Hudson Valley Ghostbusters, a 501(c)(3) non-profit, dedicated to having fun while doing good'
 THEME = 'theme'
 
@@ -59,6 +59,7 @@ SITEMAP = {
 
 DEFAULT_PAGINATION = False
 DISPLAY_CATEGORIES_ON_MENU = False
+PAGE_EXCLUDES = ['pages/thanks.html']
 PAGE_ORDER_BY = "sort_order"
 API_KEY = "3b974f3ccb23633eb1dd640aae473cd0"
 
